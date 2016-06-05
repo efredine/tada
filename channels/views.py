e2601b65cf3faba63a41e1ea385791a12a01b138
Eric <eric.fredine@gmail.com>
2016-06-05 09:35:49 -0700

    Require login for view channels.

--- channels/views.py
+++ channels/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Channel
 
-class ChannelListView(generic.ListView):
+class ChannelListView(LoginRequiredMixin, generic.ListView):
     context_object_name = 'channel_list'
-    queryset = Channel.objects.all()
     template_name = 'channels/channel_list.html'
     
-class ChannelDetailView(generic.DetailView):
+    def get_queryset(self):
+        return Channel.objects.filter(users=self.request.user).order_by('name')
+         
+class ChannelDetailView(LoginRequiredMixin, generic.DetailView):
     model = Channel
     template_name = 'channels/channel_detail.html'
