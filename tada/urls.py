b81f29798340d986d67a92b3580a141d9d2bfc24
Eric <eric.fredine@gmail.com>
2016-06-06 15:01:24 -0700

    Redirect root to channels.

--- tada/urls.py
+++ tada/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    url(r'^$', RedirectView.as_view(url='/channels/', permanent=False), name='index'),
     url(r'^channels/', include('channels.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/login/$', auth_views.login, {'template_name': 'channels/login.html'}),
